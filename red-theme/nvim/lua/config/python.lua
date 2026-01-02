762deb6d3f0523fcff3053c8d955509b9f6d9c4d
Leonardo <leonardorscarpitta@gmail.com>
2026-01-02 19:32:41 -0300

    refactor: separate themes in folders

---
+++ red-theme/nvim/lua/config/python.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = "basic",
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
