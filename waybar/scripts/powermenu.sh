63921f5c6f53daeae8d48709cf69b60350711341
Leonardo <leonardorscarpitta@gmail.com>
2026-01-29 11:06:53 -0300

    dotfiles_update_pc_2026-01-29_11-06-48

--- waybar/scripts/powermenu.sh
+++ waybar/scripts/powermenu.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-choice=$(printf "⏻  Shutdown\n  Reboot\n󰍃  Suspend\n󰌾  Lock" | \
-    rofi -dmenu -p "Power" \
-    -theme-str 'element-text { font: "JetBrainsMono Nerd Font 16"; }
+choice=$(printf "⏻  Shutdown\n  Reboot\n󰍃  Suspend\n󰌾  Lock" |
+  rofi -dmenu -p "Power" \
+    -theme-str 'element-text { font: "JetBrains Mono 12"; }
                 listview { lines: 4; }
-                element { padding: 14px; }
-                window { width: 24%; }')
+                element { padding: 16px; }
+                window { width: 25%; }')
 
 case "$choice" in
-    *Shutdown*) systemctl poweroff ;;
-    *Reboot*) systemctl reboot ;;
-    *Suspend*) hyprctl dispatch exit ;;
-    *Lock*) hyprlock ;;
+*Shutdown*) systemctl poweroff ;;
+*Reboot*) systemctl reboot ;;
+*Suspend*) hyprctl dispatch exit ;;
+*Lock*) hyprlock ;;
 esac
